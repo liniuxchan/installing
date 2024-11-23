7d591a640029f7d4570d01b924ccb9c0d25e6ac8
liniuxchan <liniuxchan@gmail.com>
2024-11-24 03:55:38 +0900

    Update r.sh
    i add geometory

--- r.sh
+++ r.sh
@@ -2,6 +2,6 @@ vncserver -kill :1
 rm /data/data/com.termux/files/usr/var/run/xrdp.pid
 rm /data/data/com.termux/files/usr/var/run/xrdp-sesman.pid
 vncserver :1
-export DISPLAY=:1
+export DISPLAY=:1 -geometry 1280x720
 xrdp&
 xrdp-sesman&
