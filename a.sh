173d4f8d0b5eac69e03a240cb6961757bdbf73e1
liniuxchan <liniuxchan@gmail.com>
2024-11-23 20:56:20 +0900

    Update a.sh
    install expect

--- a.sh
+++ a.sh
@@ -2,7 +2,7 @@ echo "Hi! I'm Liniux-chan."
 read -sp "enter password" password
 echo
 pkg update && pkg upgrade -y
-#pkg install expect -y
+pkg install expect -y
 pkg install x11-repo
 pkg install tigervnc -y
 vncserver :1 -geometry 1280x720
