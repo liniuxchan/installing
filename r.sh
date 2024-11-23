e60a9cfff841e970da6466435f8bb1278107c94c
liniuxchan <liniuxchan@gmail.com>
2024-11-24 03:57:50 +0900

    Update r.sh
    fix vncserver geometory

--- r.sh
+++ r.sh
@@ -1,7 +1,7 @@
 vncserver -kill :1
 rm /data/data/com.termux/files/usr/var/run/xrdp.pid
 rm /data/data/com.termux/files/usr/var/run/xrdp-sesman.pid
-vncserver :1
-export DISPLAY=:1 -geometry 1280x720
+vncserver :1 -geometry 1280x720
+export DISPLAY=:1
 xrdp&
 xrdp-sesman&
