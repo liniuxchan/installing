aca3be8372f0acaf1d515d1de047b426d58d37c8
liniuxchan <liniuxchan@gmail.com>
2024-11-23 21:08:10 +0900

    Update a.sh
    auto by here-document

--- a.sh
+++ a.sh
@@ -2,14 +2,10 @@ echo "Hi! I'm Liniux-chan."
 read -sp "enter password" password
 echo
 pkg update && pkg upgrade -y
-pkg install expect -y
-pkg install tcl -y
 pkg install x11-repo
 pkg install tigervnc -y
-vncserver :1 -geometry 1280x720
-expect "Password:"
-send "$password\r"
-expect "Verify:"
-send "$password\r"
-expect "view-only also?"
-send "\n"
+vncserver :1 -geometry 1280x720 <<EOF
+$password
+$password
+n
+EOF
