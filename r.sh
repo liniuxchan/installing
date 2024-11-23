4efe31f84cc09a2c4f6bb6ac738eb93b8efabd86
liniuxchan <liniuxchan@gmail.com>
2024-11-24 06:47:43 +0900

    Update r.sh

--- r.sh
+++ r.sh
@@ -2,6 +2,8 @@ export DISPLAY=:1
 rm /data/data/com.termux/files/usr/var/run/xrdp.pid
 rm /data/data/com.termux/files/usr/var/run/xrdp-sesman.pid
 vncserver -kill :1
+ps aux | grep vnc
+ps aux | grep xrdp
 vncserver :1 -geometry 1280x720
 xrdp-sesman&
 xrdp&
