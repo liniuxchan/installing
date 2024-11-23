0139df82c6324aa568e2b68d9b13e9998d29d22e
liniuxchan <liniuxchan@gmail.com>
2024-11-24 07:02:38 +0900

    Update r.sh

--- r.sh
+++ r.sh
@@ -1,9 +1,10 @@
 export DISPLAY=:1
 #pkill xrdp
 #pkill xrdp-sessman
+ps aux | grep xrdp | grep -v grep | awk '{print $2}' | xargs kill -9
 ps aux | grep vnc | grep -v grep | awk '{print $2}' | xargs kill -9
 ps aux | grep vnc
-#ps aux | grep xrdp
+ps aux | grep xrdp
 #vncserver :1 -geometry 1280x720
 #xrdp-sesman&
 #xrdp&
