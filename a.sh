a77c1996d74c4344437fda609c545d1bc625a828
liniuxchan <liniuxchan@gmail.com>
2024-11-23 20:49:55 +0900

    Update a.sh
    fix install except

--- a.sh
+++ a.sh
@@ -2,7 +2,7 @@ echo "Hi! I'm Liniux-chan."
 read -sp "enter password" password
 echo
 pkg update && pkg upgrade -y
-pkg except -y
+pkg install except -y
 pkg install x11-repo
 pkg install tigervnc -y
 vncserver :1 -geometry 1280x720
