46c673178eb08e608adbe45c94511a6f00bf39b8
liniuxchan <liniuxchan@gmail.com>
2024-11-24 03:48:19 +0900

    Update r.sh
    add export display:1

--- r.sh
+++ r.sh
@@ -2,5 +2,6 @@ vncserver -kill :1
 rm /data/data/com.termux/files/usr/var/run/xrdp.pid
 rm /data/data/com.termux/files/usr/var/run/xrdp-sesman.pid
 vncserver :1
+export DISPLAY=:1
 xrdp&
 xrdp-sesman&
