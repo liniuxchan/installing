45d9e65e106272589a155026e39cd9ad0fb2d954
liniuxchan <liniuxchan@gmail.com>
2024-11-23 20:54:56 +0900

    Update a.sh
    fix expect spell

--- a.sh
+++ a.sh
@@ -2,13 +2,13 @@ echo "Hi! I'm Liniux-chan."
 read -sp "enter password" password
 echo
 pkg update && pkg upgrade -y
-pkg install except -y
+#pkg install expect -y
 pkg install x11-repo
 pkg install tigervnc -y
 vncserver :1 -geometry 1280x720
-except "Password:"
+expect "Password:"
 send "$password\r"
-except "Verify:"
+expect "Verify:"
 send "$password\r"
-except "view-only also?"
+expect "view-only also?"
 send "\n"
