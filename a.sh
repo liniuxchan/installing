099bdb22e8d46bb853d19620aa53655f6c9e8f0c
liniuxchan <liniuxchan@gmail.com>
2024-11-23 20:42:51 +0900

    Update a.sh
    autonize password

--- a.sh
+++ a.sh
@@ -1,6 +1,12 @@
 echo "Hi! I'm Liniux-chan."
 pkg update && pkg upgrade -y
+pkg except -y
 pkg install x11-repo
 pkg install tigervnc -y
-termux-desktop passwd
 vncserver :1 -geometry 1280x720
+except "Password:"
+send "$password\r"
+except "Verify:"
+send "$password\r"
+except "view-only also?"
+send "\n"
