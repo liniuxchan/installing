55c68349364ca8bc11f3228b9673678cdd6fae40
liniuxchan <liniuxchan@gmail.com>
2024-11-24 09:21:17 +0900

    Update x.sh

--- x.sh
+++ x.sh
@@ -17,9 +17,7 @@ n
 EOF
 vncserver -kill :1
 vncserver :1 -geometry 1280x720
-pkg install aterm -y
 export DISPLAY=:1
-aterm -geometry 80x80+100+100&
 pkg install xrdp -y
 pkg install libcrypt -y
 xrdp
