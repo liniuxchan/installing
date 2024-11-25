2df9d569c9170535c2831abb7334fb3c9067a9be
liniuxchan <liniuxchan@gmail.com>
2024-11-26 01:47:50 +0900

    Update f.sh

--- f.sh
+++ f.sh
@@ -8,6 +8,10 @@ read -s liniuxchan_your_vnc_password
 # パスワードを確認
 echo "入力されたパスワードは変数に保存されました"
 
+openssl genpkey -algorithm RSA -out private.key -aes256
+openssl req -new -key private.key -out request.csr
+openssl x509 -req -in request.csr -signkey private.key -out certificate.crt -days 365
+
 unset liniuxchan_your_vnc_password
 set
 set +x
