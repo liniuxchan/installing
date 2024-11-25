5fb7d7d35e14b4ec486b91114db838dced50800a
liniuxchan <liniuxchan@gmail.com>
2024-11-26 02:59:41 +0900

    Update f.sh

--- f.sh
+++ f.sh
@@ -8,8 +8,8 @@ read -s liniuxchan_your_vnc_password
 # パスワードを確認
 echo "入力されたパスワードは変数に保存されました"
 mkdir ~/.vnc
-openssl req -x509 -newkey rsa:4096 -keyout ~/.vnc/vncserver.key -out ~/.vnc/vncserver.crt -days 365 -nodes
-printf 'SecurityTypes=vencrypt,x509vnc\nX509Key=~/.vnc/myvnc.key\nX509Cert=~/.vnc/myvnc.crt'　> ~/.vnc/config
+openssl req -x509 -newkey rsa:4096 -keyout ~/.vnc/vncserver.key -out ~/.vnc/vncserver.crt -days 365 -nodes -subj "/C=JP/ST=Tokyo/L=Shibuya/O=Liniux/OU=IT/CN=liniuxchan"
+printf 'SecurityTypes=vencrypt,x509vnc\nX509Key=~/.vnc/myvnc.key\nX509Cert=~/.vnc/myvnc.crt'　> ~/.vnc/configLiniux
 vncsever :1
 unset liniuxchan_your_vnc_password
 #set
