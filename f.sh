4aa446ed1824e9e4e11638064f2884720f55bfe1
liniuxchan <liniuxchan@gmail.com>
2024-11-26 01:27:55 +0900

    Update f.sh

--- f.sh
+++ f.sh
@@ -1,2 +1,9 @@
 pkg install x11-repo && pkg update
 pkg install tigervnc fluxbox firefox -y && pkg upgrade -y
+
+# パスワードを非表示で入力させて変数に保存
+echo "パスワードを入力してください:"
+read -s liniuxchan-vnc-password
+
+# パスワードを確認
+echo "入力されたパスワードは変数に保存されました"
