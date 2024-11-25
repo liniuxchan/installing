c0d42ba84eb9d729bdc250113d39db0c068b038f
liniuxchan <liniuxchan@gmail.com>
2024-11-26 05:33:55 +0900

    Update f.sh

--- f.sh
+++ f.sh
@@ -1,7 +1,4 @@
-pkg install x11-repo && pkg update
-pkg install openssl-tool tigervnc fluxbox firefox -y && pkg upgrade -y
-#set -x
-# パスワードを非表示で入力させて変数に保存
+
 echo "🥳Hello everyone! I'm Liniuxchan!"
 while true; do
   read -p "🤗Tell me your vnc-password! :" password
@@ -13,7 +10,10 @@ while true; do
     echo "😚6 or more!"
   fi
 done
-
+pkg install x11-repo && pkg update
+pkg install openssl-tool tigervnc fluxbox firefox -y && pkg upgrade -y
+#set -x
+# パスワードを非表示で入力させて変数に保存
 mkdir ~/.vnc
 openssl req -x509 -newkey rsa:4096 -keyout ~/.vnc/vncserver.key -out ~/.vnc/vncserver.crt -days 365 -nodes -subj "/C=JP/ST=vurtualTokyo/L=vurtual Shibuya/O=Liniuxchan and Friends/OU=playing with linux/CN=liniuxchan on your phone"
 printf "SecurityTypes=vencrypt,x509vnc\nX509Key=~/.vnc/vncserver.key\nX509Cert=~/.vnc/vncserver.crt\n" >> ~/.vnc/config
