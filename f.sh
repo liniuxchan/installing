c15950299a2a152b4b4150faaa05bca8dfaad82a
liniuxchan <liniuxchan@gmail.com>
2024-11-26 07:59:14 +0900

    Update f.sh

--- f.sh
+++ f.sh
@@ -20,6 +20,7 @@ rm ~/.vnc/config
 printf "SecurityTypes=X509Vnc\nX509Key=/data/data/com.termux/files/home/.vnc/vncserver.key\nX509Cert=/data/data/com.termux/files/home/.vnc/vncserver.crt\n" >> ~/.vnc/config
 echo $password
 # 自動的にパスワードを設定
+rm /data/data/com.termux/files/usr/tmp/.X1-lock
 pkill vnc
 echo -e "$password\n$password\nn" | vncserver :1
 unset liniuxchan_your_vnc_password
